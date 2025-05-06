3059fb0e0a862a2a8d687e6cd543b52bb0dda8fe
Adam Jensen <16873632+adamjosephjensen@users.noreply.github.com>
2025-05-06 11:51:40 -0500

    ugly first attempt at ValidSudoku

--- ValidSudoku.py
+++ ValidSudoku.py
@@ -44,8 +44,65 @@
 from typing import List
 
 class Solution:
+    @staticmethod
+    def parse_int_or_none(s):
+        try:
+            return int(s)
+        except (ValueError, TypeError):
+            return None
+
+    @staticmethod
+    def check_row(s: List[str]) -> bool:
+        print("LOG check_row | row: ", s)
+        row = [i for i in [Solution.parse_int_or_none(x) for x in s] if i]
+        
+        # check duplicates
+        as_set = frozenset(row)
+        return len(row) == len(as_set)
+    
+    @staticmethod
+    def check_box(box: List[List[str]]) -> bool:
+        nums = []
+        for row in range(3):
+            for col in range(3):
+                val = box[row][col]
+                if val in '0123456789':
+                    nums.append(int(val))
+
+        as_set = frozenset(nums)
+        return len(as_set) == len(nums)
+
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        pass
+        rows = [Solution.check_row(row) for row in board]
+        if False in rows:
+            return False
+
+        cols = []
+        for slow in range(9):
+            is_valid_column = Solution.check_row([board[f][slow] for f in range(9)])
+            cols.append(is_valid_column)
+        if False in cols:
+           return False
+
+        # check nine three by three grids
+        top_left = [board[r][:3] for r in range(0, 3)]
+        mid_left = [board[r][:3] for r in range(3, 6)]
+        bot_left = [board[r][:3] for r in range(6, 9)]
+
+        top_midd = [board[r][3:6] for r in range(0, 3)]
+        mid_midd = [board[r][3:6] for r in range(3, 6)]
+        bot_midd = [board[r][3:6] for r in range(6, 9)]
+
+        top_righ = [board[r][6:9] for r in range(0, 3)]
+        mid_righ = [board[r][6:9] for r in range(3, 6)]
+        bot_righ = [board[r][6:9] for r in range(6, 9)]
+        
+        sub_grids = [top_left, mid_left, bot_left, top_midd, mid_midd, bot_midd, top_righ, mid_righ, bot_righ]
+        valid_grids = [Solution.check_box(g) for g in sub_grids]
+        if False in valid_grids:
+            return False
+
+        return True
 
 import pytest
 from typing import List
