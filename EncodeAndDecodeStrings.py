09061811d8faa89337477df167bbd7a46932cabc
Adam Jensen <16873632+adamjosephjensen@users.noreply.github.com>
2025-05-06 13:53:19 -0500

    feat: Add Encode and Decode Strings solution

---
+++ EncodeAndDecodeStrings.py
@@ -0,0 +1,51 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        """ prepend |n before each string where n is the length
+        of the string
+        """
+        encoding = ""
+        for s in strs:
+            encoding += "|" + str(len(s))
+            encoding += s
+
+        return encoding
+
+    def decode(self, s: str) -> List[str]:
+        """ expect one string with |n before each sub where n is the length
+        of the substring
+        return a list of strings.
+        "|5hello|5world" returns ["hello", "world"]
+        "|0" returns [""]
+        """
+        pass
+
+import pytest
+
+# Test none, one, many, delim
+def test_encoding_none():
+    solution = Solution()
+    s = ""
+    r = solution.encode([s])
+    assert r = "|0"
+
+def test_encoding_one():
+    solution = Solution()
+    s = "hello"
+    r = solution.encode([s])
+    assert r == "|5hello"
+
+def test_encoding_many():
+    solution = Solution()
+    s = ["hello", "world"]
+    r = solution.encode(s)
+    assert r == "|5hello|5world" 
+
+def test_decoding_none():
+    pass
+
+def test_encoding_one():
+    pass
+
+def test_encoding_many():
+    pass
