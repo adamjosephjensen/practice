06fc19c0df27c7d8e4ff9afbbad6e2b7fac58859
Adam Jensen <16873632+adamjosephjensen@users.noreply.github.com>
2025-09-30 12:33:05 -0500

    minor changes

--- EncodeAndDecodeStrings.py
+++ EncodeAndDecodeStrings.py
@@ -1,6 +1,55 @@
 from typing import List
 
-class Solution:
+class _2025_09_30_Solution:
+    @staticmethod
+    def delimeter():
+        return "||"
+
+    def encode(self, strs: List[str]) -> str:
+        """
+        Encode a lit of strings such that it can be decoded later
+
+        Key idea: need a delimeter plus a number of characters to scan
+        Input: ["hello", "world", "longstring"]
+        Output: "5||hello5||world10||longstring"
+        """
+        output = ""
+
+        for s in strs:
+            l = len(s)
+            # encode length + delimeter + string
+            encoding = str(l) + Solution.delimeter() + s
+            output = output + encoding
+        
+        print(f"output: {output}")
+        return output
+
+    def decode(self, s: str) -> List[str]:
+        """
+        decode a list of strings
+        Input: "5||hello"
+        Output: ["hello"]
+        Input: "5||hello10||longstring"
+        Output:["hello", "longstring"]
+        """
+        n = len(s)
+        cursor = 0
+        output: List[str] = []
+
+        while cursor < n:
+            # find length of string
+            digits = s[cursor:].find(Solution.delimeter())
+            _length = int(s[cursor:cursor+digits])
+            cursor += digits + len(Solution.delimeter())
+            # read string
+            cur = s[cursor:cursor+_length]
+            output.append(cur)
+            # advance cursor
+            cursor += _length
+
+        return output
+
+class OldSolution:
 
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings into a single string with length||string format."""
@@ -18,3 +67,5 @@ class Solution:
             result.append(encoded[j+2:j+2+length])
             i = j + 2 + length
         return result
+
+Solution = _2025_09_30_Solution
